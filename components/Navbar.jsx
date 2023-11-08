43a3fa7c70265d5734710b0b3f0bad07aab21d14
aluca94@gmail.com <alutza94@gmail.com>
2023-11-08 18:01:19 +0100

    add navbar links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { headerLogo } from "@/public/images";
 import Image from "next/image";
+import { navLinks } from "@/constants";
+import { hamburger } from "@/public/icons";
 
 const Navbar = () => {
   return (
@@ -8,6 +10,23 @@ const Navbar = () => {
         <a href='/'>
           <Image src={headerLogo} alt='logo' width={130} height={29} />
         </a>
+
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className='fonts-montserrat leading-normal text-lg text-slate-gray hover:underline hover:text-slate-800  
+'
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='hidden max-lg:block'>
+          <Image src={hamburger} alt='hamburger' width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
