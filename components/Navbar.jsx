cd148285f40a503c9e218197af5f797fc21221dd
aluca94@gmail.com <alutza94@gmail.com>
2023-11-08 17:46:26 +0100

    add logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { headerLogo } from "@/public/images";
+import Image from "next/image";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <Image src={headerLogo} alt='logo' width={130} height={29} />
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
