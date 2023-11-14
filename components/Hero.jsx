54705455e2ba6ac5cf07a12500c6415b6e65ce72
aluca94@gmail.com <alutza94@gmail.com>
2023-11-14 18:28:13 +0100

    fix error

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
       <div className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  '>
         <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 '>
           <p className='text-xl font-montserrat text-red-500'>
-            Our Summer collection
+            Our Autumn collection
           </p>
           <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
             <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
