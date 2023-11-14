1e8bee42330176c064e517f97d3f885a536bd22b
aluca94@gmail.com <alutza94@gmail.com>
2023-11-14 18:22:41 +0100

    add basic footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='bg-black padding-x padding-t pb-8 text-white'>
-      Footer
+    <footer className='bg-black padding-x padding-t pb-8 text-white '>
+      <div className='flex justify-center items-center'>
+        &copy; 2023{" "}
+        <a
+          className='px-1'
+          href='https://www.linkedin.com/in/luka-tizi%C4%87-529a1b240/'
+        >
+          Luka
+        </a>
+        {"  "}| All rights reserved
+      </div>
     </footer>
   );
 };
